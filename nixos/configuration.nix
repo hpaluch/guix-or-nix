e381804d4ec892b1eabd9b747f2dc647dafdce04
Henryk Paluch <hpaluch@seznam.cz>
2026-05-21 21:01:15 +0200

    nixos: adding system-wide: git mc tmux vim

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -96,8 +96,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+     # note: rsync is already included in base system
+     git mc tmux vim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
